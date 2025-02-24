c4127df2b6cac137aaf4bdc7d1df090b7bfc1434
dknathalage <dknathalage@gmail.com>
2025-02-24 22:25:21 +1100

    remove unnecessary comments

--- log/log_test.go
+++ log/log_test.go
@@ -8,28 +8,21 @@ import (
 )
 
 func TestNewJsonLogger(t *testing.T) {
-	// Create a pipe
 	r, w, _ := os.Pipe()
 
-	// Save the original stderr
 	oldStderr := os.Stderr
 
-	// Redirect stderr to our pipe
 	os.Stderr = w
 
-	// Call the logger
 	logger := NewJsonLogger()
 	logger.Info("Test log message")
 
-	// Close writer to flush data and restore stderr
 	w.Close()
 	os.Stderr = oldStderr
 
-	// Read output from pipe
 	var buf bytes.Buffer
 	_, _ = buf.ReadFrom(r)
 
-	// Log and check the output
 	logOutput := buf.String()
 
 	if !strings.Contains(logOutput, "Test log message") {
